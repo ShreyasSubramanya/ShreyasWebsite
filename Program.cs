f6a888a82851e0eed616b075e059e7ebc9c19002
Shreyas B Subramanya <shreyassubramanya@Shreyass-MacBook-Air-2.local>
2025-05-17 18:42:12 -0700

    Updated homepage message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "This is Shreyas's website!");
 
 app.Run();
+dotnet publish -c Release -o ./publish
